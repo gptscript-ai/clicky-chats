3d216530a3233bd80e0ef5bfe989390ea4c357d8
Darren Shepherd <darren@acorn.io>
2024-04-03 20:39:19 -0700

    change: launch clicky chats and knowledge as tools

--- pkg/knowledgebases/manager.go
+++ pkg/knowledgebases/manager.go
@@ -1,7 +1,14 @@
 package knowledgebases
 
 import (
+	"context"
+	"log/slog"
+	"os"
+	"strings"
+
 	"github.com/gptscript-ai/clicky-chats/pkg/db"
+	"github.com/gptscript-ai/gptscript/pkg/gptscript"
+	"github.com/gptscript-ai/gptscript/pkg/loader"
 )
 
 type Config struct {
@@ -13,9 +20,30 @@ type KnowledgeBaseManager struct {
 	db *db.DB
 }
 
-func NewKnowledgeBaseManager(config Config, db *db.DB) *KnowledgeBaseManager {
+func NewKnowledgeBaseManager(ctx context.Context, config Config, db *db.DB) (*KnowledgeBaseManager, error) {
+	if !strings.HasPrefix(config.KnowledgeRetrievalAPIURL, "http") {
+		url, err := launchKnowledge(ctx, config.KnowledgeRetrievalAPIURL)
+		if err != nil {
+			return nil, err
+		}
+		slog.Info("Launched knowledge retrieval API", "url", url)
+		config.KnowledgeRetrievalAPIURL = url
+	}
 	return &KnowledgeBaseManager{
 		Config: config,
 		db:     db,
+	}, nil
+}
+
+func launchKnowledge(ctx context.Context, tool string) (string, error) {
+	prg, err := loader.Program(ctx, tool, "")
+	if err != nil {
+		return "", err
 	}
+	prg = prg.SetBlocking()
+	g, err := gptscript.New(nil)
+	if err != nil {
+		return "", err
+	}
+	return g.Run(ctx, prg, os.Environ(), "")
 }
